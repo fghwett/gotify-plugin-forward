d2478db6682a3e888cdebf105b1bb1fd26ef0ca7
lightos-arch <lightos-arch@aweoo.com>
2026-09-01 10:36:44 +0800

    发布 v0.1.0

--- consts/consts.go
+++ consts/consts.go
@@ -2,7 +2,7 @@ package consts
 
 const (
 	PluginModulePath  = "github.com/fghwett/gotify-plugin-forward"
-	PluginVersion     = "0.0.3"
+	PluginVersion     = "0.1.0"
 	PluginAuthor      = "FGHWETT"
 	PluginWebsite     = "https://github.com/fghwett/gotify-plugin-forward"
 	PluginDescription = "将消息转发到bark等平台"
